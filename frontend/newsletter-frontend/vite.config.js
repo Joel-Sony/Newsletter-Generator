f23a56a48f02834f681089f8c49c93310f383afb
Joel Sony <joelsonytab@gmail.com>
2025-06-09 14:08:55 +0530

    Fixed the editor bugs to work with flask

--- frontend/newsletter-frontend/vite.config.js
+++ frontend/newsletter-frontend/vite.config.js
@@ -1,14 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'  
 
 export default defineConfig({
-  base: "/editor",
-  plugins: [
-    react(),
-    tailwindcss(), 
-  ],
+  plugins: [react()],
   server: {
-    open: true,
-  },
+    proxy: {
+      '/api': 'http://localhost:5000'
+    }
+  }
 })
